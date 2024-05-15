e70d26888aa089dcbe9cacbd032eff9d696229de
EdP23 <edgars.petrackovs@dci-student.org>
2024-05-15 14:30:50 +0200

    änderung

--- Finanz-Tracker/src/App.jsx
+++ Finanz-Tracker/src/App.jsx
@@ -1,16 +1,14 @@
 import Tracker from "./components/Tracker";
-import React, { useState, useEffect } from "react";
 
 import "./App.css";
 
 //Die App-Komponente wird in eine eigene Datei ausgelagert
 const App = () => {
-
   return (
     <div>
       <Tracker />
     </div>
   );
-}
+};
 
 export default App;
