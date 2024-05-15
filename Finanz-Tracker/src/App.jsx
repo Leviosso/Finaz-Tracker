f77b3103e253b3dc59c727e248919c6a045eb972
Oleksandr <oleksandr.zhemchuzhnyi@dci-student.com>
2024-05-15 14:43:50 +0200

    Test kommentar

--- Finanz-Tracker/src/App.jsx
+++ Finanz-Tracker/src/App.jsx
@@ -3,6 +3,7 @@ import Tracker from "./components/Tracker";
 import "./App.css";
 
 //Die App-Komponente wird in eine eigene Datei ausgelagert
+//und die Tracker-Komponente wird in die App-Komponente importiert
 const App = () => {
 
   return (
